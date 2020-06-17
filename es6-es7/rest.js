ec2b4286975052a75882f25bd35422d45eb04610
love596310898 <596310898@163.com>
2020-06-17 18:40:00 +0800

    set 练习

--- es6-es7/rest.js
+++ es6-es7/rest.js
@@ -1,10 +1,10 @@
 // 扩展运算符rest
 /**
  * 函数形参的合并，函数实参的展开
- * 数组的解构赋值，数组的合并，解构合并的是数组内的元素，是枚举操作（数组中元素均以下标为key保存，枚举时以下标做为key枚举）
- * 对象的解构赋值，对象的合并，解构合并的是对象的可枚举属性, 是枚举操作
+ * 数组的展开 迭代操作 展开的是数组的元素成员
+ * 对象的展开，枚举操作，展开的是对象的可枚举自有属性
  * rest的合并规则共object.assign函数
- * rest对数组和对象的操作均为枚举操作
+ * rest对于有迭代器的对象采用迭代操作，没有迭代器的对象则采取枚举操作
  */
 
 // 对于存取器属性的rest使用
@@ -22,7 +22,7 @@ Object.defineProperty(a, 'i', {
   value: 1,
   configurable: true,
   writable: true,
-  enumerable: true,
+  enumerable: false,
 });
 const b = new Set([1, 2]);
 const c = new Map([[0, 1], [1, 2]]);
@@ -30,5 +30,4 @@ const d = [1, 2];
 const e = {
   name: 'b', ...a, ...b, ...c, ...d,
 };
-global.console.log(Object.getOwnPropertyDescriptors(d));
 global.console.log(a, b, c, d, e);
--- es6-es7/set-and-map.js
+++ es6-es7/set-and-map.js
@@ -1,11 +1,12 @@
 /**
  * set所有成员不重复 重复的判断是===运算  NaN例外 在set中NaN与自身相等
  * set实例基础方法 add（增加） 、delet（删除） 、has（是否存在） 、clear（清空）
+ * set类部署了interator接口 可使用keys 、 values、 entries 获取迭代器  也可使用forEach 直接迭代
+ * set可直接使用for of 循环
+ * set迭代结果不是键值对结构，虽然entries返回迭代结果为[element1, element2]结构，但两个值相等都是其成员的value
+ * set有迭代器接口可以使用rest操作符 将其展开到Array中
+ * 将set使用rest操作符展开到Object中时 由于不返回键值对 相当于展开了一个空对象{}
  */
 const set = new Set([1, 2]);
-set.add(3);
-global.console.log(set);
-set.add(3);
-global.console.log(set);
-set.delete(2);
-global.console.log({ ...set });
+const a = { ...set };
+global.console.log(a);
