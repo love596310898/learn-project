a99c9e93e863b84c7eda712895974a274dcbef49
love596310898 <596310898@163.com>
2020-07-01 18:35:13 +0800

    vite项目创建 错误处理完善 内存泄漏整理 数据类型转换整理

---
+++ vue/vue-demo-vite-cli/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="count++">
+      count is: {{ count }}
+    </button>
+    <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+    <TextComponent />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  components: {
+    TextComponent: {
+      template: '<p>{{name}}</p>',
+      data: () => ({ name: 'p' }),
+    },
+  },
+  props: {
+    msg: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
