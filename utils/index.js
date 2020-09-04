0296f8547d557ef26196f6425a379fa7ba94fb5f
love596310898 <596310898@163.com>
2020-09-04 18:29:41 +0800

    添加工具函数

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,16 @@
-// 用于下载
+// 转化stream为可访问的url
+const streamToUrl = (data) => {
+  if (!Buffer.isBuffer(data)) {
+    throw new Error('allowable type is arraybuffer');
+  }
+  const blob = new Blob([data], { type: 'application/octet-stream' });
+  // 兼容不同浏览器的URL对象
+  const url = window.URL || window.webkitURL || window.moxURL;
+  // 创建下载链接
+  return url.createObjectURL(blob);
+};
+
+// 下载文件
 const downloadFileByUrl = ({ url, fileName }) => {
   if ('download' in document.createElement('a')) {
     const elink = document.createElement('a');
