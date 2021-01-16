c28bfbad79afb2383fe1372b16fa90d4b8f9bcd1
yanyun <596310898@163.com>
2021-01-16 12:40:58 +0800

    configure code formatter and linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,50 @@
 module.exports = {
-  root: true, // 从根目录进行校验
-  env: { // 执行环境
-    browser: true, // 浏览器
-    node: true, // nodeJs
-    es6: true, // EACMscript6
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
-  // 扩展
-  extends: ["plugin:prettier/recommended"],
-  // 指定忽略的文件
+  extends: [
+    'plugin:vue/essential', // 必须遵守的
+    // 'plugin:vue/strongly-recommended', // 强烈推荐的
+    // 'plugin:vue/recommended', // 推荐的
+    '@vue/airbnb', // 爱彼迎风格规范
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    parser: 'babel-eslint',
+  },
   ignorePatterns: ["**/dist/**/*.js", "**/.vscode/*.js"],
-  // 自定义规则
-  rules: {},
-  // 指定处理的文件范围
+  rules: {
+    "indent": ["error", 4],
+    'max-classes-per-file': ['off'],
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'max-len': ['error', 200],
+    'consistent-return': ['off'],
+    'import/extensions': ['off'],
+    'import/no-unresolved': ['off'],
+    'import/prefer-default-export': ['off'],
+    'import/no-extraneous-dependencies': ['off'],
+    'func-names': ['off'],
+    'linebreak-style': ['off'],
+    "no-unused-vars": ["off"],
+    'vue/eqeqeq': ['error', 'always'],
+    'vue/key-spacing': ['error', {
+      mode: 'strict',
+      align: {
+        beforeColon: false,
+        afterColon: true,
+        on: 'colon',
+      },
+    }],
+  },
   overrides: [{
     files: [
-      "**/__tests__/*.{j,t}s?(x)",
-      "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      '**/__tests__/*.{j,t}s?(x)',
+      '**/tests/unit/**/*.spec.{j,t}s?(x)',
     ],
     env: {
       mocha: true,
--- echarts/map/map.config.js
+++ echarts/map/map.config.js
@@ -1,150 +1,150 @@
-var keyMap = [
-	{
-		label: '中国',
-		value: 'china'
-	},
-	{
-		label: '世界',
-		value: 'world'
-	},
-	{
-		label: '世界中国',
-		value: 'world-china'
-	},
-	{
-		label: '安徽',
-		value: 'anhui'
-	},
-	{
-		label: '澳门',
-		value: 'aomen'
-	},
-	{
-		label: '北京',
-		value: 'beijing'
-	},
-	{
-		label: '重庆',
-		value: 'chongqing'
-	},
-	{
-		label: '福建',
-		value: 'fujian'
-	},
-	{
-		label: '甘肃',
-		value: 'gansu'
-	},
-	{
-		label: '广东',
-		value: 'guangdong'
-	},
-	{
-		label: '广西',
-		value: 'guangxi'
-	},
-	{
-		label: '贵州',
-		value: 'guizhou'
-	},
-	{
-		label: '海南',
-		value: 'hainan'
-	},
-	{
-		label: '河北',
-		value: 'hebei'
-	},
-	{
-		label: '黑龙江',
-		value: 'heilongjiang'
-	},
-	{
-		label: '河南',
-		value: 'henan'
-	},
-	{
-		label: '湖北',
-		value: 'hubei'
-	},
-	{
-		label: '湖南',
-		value: 'hunan'
-	},
-	{
-		label: '江苏',
-		value: 'jiangsu'
-	},
-	{
-		label: '江西',
-		value: 'jiangxi'
-	},
-	{
-		label: '吉林',
-		value: 'jilin'
-	},
-	{
-		label: '辽宁',
-		value: 'liaoning'
-	},
-	{
-		label: '内蒙古',
-		value: 'neimenggu'
-	},
-	{
-		label: '宁夏',
-		value: 'ningxia'
-	},
-	{
-		label: '青海',
-		value: 'qinghai'
-	},
-	{
-		label: '山东',
-		value: 'shandong'
-	},
-	{
-		label: '上海',
-		value: 'shanghai'
-	},
-	{
-		label: '山西',
-		value: 'shanxi'
-	},
-	{
-		label: '陕西',
-		value: 'shanxi1'
-	},
-	{
-		label: '四川',
-		value: 'sichuan'
-	},
-	{
-		label: '台湾',
-		value: 'taiwan'
-	},
-	{
-		label: '天津',
-		value: 'tianjin'
-	},
-	{
-		label: '香港',
-		value: 'xianggang'
-	},
-	{
-		label: '新疆',
-		value: 'xinjiang'
-	},
-	{
-		label: '西藏',
-		value: 'xizang'
-	},
-	{
-		label: '云南',
-		value: 'yunnan'
-	},
-	{
-		label: '浙江',
-		value: 'zhejiang'
-	}
+const keyMap = [
+    {
+        label: '中国',
+        value: 'china',
+    },
+    {
+        label: '世界',
+        value: 'world',
+    },
+    {
+        label: '世界中国',
+        value: 'world-china',
+    },
+    {
+        label: '安徽',
+        value: 'anhui',
+    },
+    {
+        label: '澳门',
+        value: 'aomen',
+    },
+    {
+        label: '北京',
+        value: 'beijing',
+    },
+    {
+        label: '重庆',
+        value: 'chongqing',
+    },
+    {
+        label: '福建',
+        value: 'fujian',
+    },
+    {
+        label: '甘肃',
+        value: 'gansu',
+    },
+    {
+        label: '广东',
+        value: 'guangdong',
+    },
+    {
+        label: '广西',
+        value: 'guangxi',
+    },
+    {
+        label: '贵州',
+        value: 'guizhou',
+    },
+    {
+        label: '海南',
+        value: 'hainan',
+    },
+    {
+        label: '河北',
+        value: 'hebei',
+    },
+    {
+        label: '黑龙江',
+        value: 'heilongjiang',
+    },
+    {
+        label: '河南',
+        value: 'henan',
+    },
+    {
+        label: '湖北',
+        value: 'hubei',
+    },
+    {
+        label: '湖南',
+        value: 'hunan',
+    },
+    {
+        label: '江苏',
+        value: 'jiangsu',
+    },
+    {
+        label: '江西',
+        value: 'jiangxi',
+    },
+    {
+        label: '吉林',
+        value: 'jilin',
+    },
+    {
+        label: '辽宁',
+        value: 'liaoning',
+    },
+    {
+        label: '内蒙古',
+        value: 'neimenggu',
+    },
+    {
+        label: '宁夏',
+        value: 'ningxia',
+    },
+    {
+        label: '青海',
+        value: 'qinghai',
+    },
+    {
+        label: '山东',
+        value: 'shandong',
+    },
+    {
+        label: '上海',
+        value: 'shanghai',
+    },
+    {
+        label: '山西',
+        value: 'shanxi',
+    },
+    {
+        label: '陕西',
+        value: 'shanxi1',
+    },
+    {
+        label: '四川',
+        value: 'sichuan',
+    },
+    {
+        label: '台湾',
+        value: 'taiwan',
+    },
+    {
+        label: '天津',
+        value: 'tianjin',
+    },
+    {
+        label: '香港',
+        value: 'xianggang',
+    },
+    {
+        label: '新疆',
+        value: 'xinjiang',
+    },
+    {
+        label: '西藏',
+        value: 'xizang',
+    },
+    {
+        label: '云南',
+        value: 'yunnan',
+    },
+    {
+        label: '浙江',
+        value: 'zhejiang',
+    },
 ];
