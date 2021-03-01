2e179132074fb9ca1b785868938849c019539c99
yanyun <596310898@163.com>
2021-03-01 18:41:39 +0800

    正则表达式理解更新

--- utils/RegExp.js
+++ utils/RegExp.js
@@ -1,5 +1,12 @@
 // 数字每隔三位加一个逗号
 const regexp1 = /(\d+?)(?=(?:\d{3})+$)/g;
-console.log('1111111111111111111'.replace(regexp1, '$1,'));
+console.log('111111111'.replace(regexp1, '$1,'));
 // 整数或者保留两位小数
-const regexp2 = /^\d*(\.\d{1,2})?$/;
+const regexp2 = /^(\d*(\.\d{1,2})?)\d*$/;
+console.log('111.111'.replace(regexp2, '$1'))
+// 手机号脱敏
+const regexp3 = /(?<=\d{3})\w(?=\w{4})/g;
+console.log('18372634789'.replace(regexp3, '*'));
+// 身份证号脱敏
+const regexp4 = /(?<=\d{4})\w(?=\w{4})/g;
+console.log('420321198909263818'.replace(regexp4, '*'))
